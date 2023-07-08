980334fdbfd75e0e04a9ef0fa0f9a8dd5d42b7d3
Ali Bagci <abagci.phys@gmail.com>
2023-07-08 22:44:55 +0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,12 @@ using HyperRF
 using Test
 
 @testset "HyperRF.jl" begin
-    # Write your tests here.
+    HypREval(HyperRadialF(OTwoEeL, 1), 5,RF(15//10),RF(5//10),RF(16//10),RF(6//10))
+    HypREval(HyperRadialF(OTwoEgL, 1), 5,RF(15//10),RF(5//10),RF(16//10),RF(6//10))
+    HypREval(HyperRadialF(OTwoEhL, 1), 5,RF(15//10),RF(5//10),RF(16//10),RF(6//10))
+    HypREval(HyperRadialF(OTwoElL, 1), 5,RF(15//10),RF(5//10),RF(16//10),RF(6//10))
+    HypREval(HyperRadialF(HyperRLTest, 1), 5,RF(15//10),RF(5//10),RF(16//10),RF(6//10))
+    HypREval(HyperRadialF(HyperRL0, 1), RF(15//10),RF(5//10),RF(16//10),RF(6//10))
+    HypREval(HyperRadialF(HyperRL1, 1), RF(15//10),RF(5//10),RF(16//10),RF(6//10))
+    HypREval(HyperRadialF(OTwoERL, 1), RF(15//10),RF(5//10),RF(16//10),RF(6//10))
 end
