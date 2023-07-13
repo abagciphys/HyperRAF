fee359fc1c0ce37c5a14d27c36484b6f93ed8e69
Ali Bagci <abagci.phys@gmail.com>
2023-07-14 00:29:03 +0300

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -19,7 +19,7 @@ function HyperRaFT(HyperD::HyperRaFD, L::Int, n1::arb, ρ1::arb, n2::arb, ρ2::a
     file = open(file_path, "w")
 
     total_time = 0.0
-    for s in 1:L
+    for s in 0:L
         time = @elapsed HyperD.HyperD(HyperD.mode, L, n1, ρ1, n2, ρ2)
         total_time += time
         println(file, "$s\t$time\t$total_time")  # Write time and total_time in two separate columns
