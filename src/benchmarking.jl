ee23fcbc8cefd3d9dbadc1cf5ae1d65fe07f669b
Ali Bagci <abagci.phys@gmail.com>
2023-07-14 02:13:07 +0300

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -14,16 +14,32 @@ end
 
 function HyperRaFT(HyperD::HyperRaFD, L::Int, n1::arb, ρ1::arb, n2::arb, ρ2::arb)
     user_home_dir = homedir()
-    file_name = "cpu_$(HyperD.mode)_time.dat"
-    file_path = joinpath(user_home_dir, file_name)
-    file = open(file_path, "w")
+    file_time = "cpu_$(HyperD.mode)_time.dat"
+    file_val = "cpu_$(HyperD.mode)_val.dat"
+    file_tpath = joinpath(user_home_dir, file_time)
+    file_vpath = joinpath(user_home_dir, file_val)
+    filet = open(file_tpath, "w")
+    filev = open(file_vpath, "w")
 
     total_time = 0.0
-    for s in 0:L
-        time = @elapsed HyperD.HyperD(HyperD.mode, L, n1, ρ1, n2, ρ2)
-        total_time += time
-        println(file, "$s\t$time\t$total_time")  # Write time and total_time in two separate columns
+    if HyperD.mode == :test
+        for s in 0 : L
+            time = @elapsed HyperD.HyperD(HyperD.mode, s, n1, ρ1, n2, ρ2)
+            val = HyperD.HyperD(HyperD.mode, s, n1, ρ1, n2, ρ2)
+            println(filet, "$s\t$time\t$total_time")
+            total_time += time
+        end
+    elseif HyperD.mode == :use
+        val = HyperD.HyperD(HyperD.mode, L, n1, ρ1, n2, ρ2)
+        for (index, value) in enumerate(val)
+            println(filev, "$index\t$value")
+        end
+        for s in 0 : L
+            time = @elapsed HyperD.HyperD(HyperD.mode, s, n1, ρ1, n2, ρ2)
+            println(filet, "$s\t$time\t$total_time")
+            total_time += time
+        end 
     end
-
-    close(file)
+    close(filet)
+    close(filev)
 end
