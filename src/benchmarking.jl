951f99d8c103b7d8ae903c99cf8e94c7e3f9fa7f
Ali Bagci <abagci.phys@gmail.com>
2023-07-14 00:15:38 +0300

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -12,7 +12,7 @@ struct HyperRaFD
     mode::Symbol
 end
 
-function HyperRaFT(HyperD::HyperRaFD, mode::Symbol, L::Int, n1::arb, ρ1::arb, n2::arb, ρ2::arb)
+function HyperRaFT(HyperD::HyperRaFD, L::Int, n1::arb, ρ1::arb, n2::arb, ρ2::arb)
     user_home_dir = homedir()
     file_name = "cpu_$(HyperD.mode)_time.dat"
     file_path = joinpath(user_home_dir, file_name)
@@ -20,7 +20,7 @@ function HyperRaFT(HyperD::HyperRaFD, mode::Symbol, L::Int, n1::arb, ρ1::arb, n
 
     total_time = 0.0
     for s in 1:L
-        time = @elapsed HyperD.HyperD(:mode, L, n1, ρ1, n2, ρ2)
+        time = @elapsed HyperD.HyperD(HyperD.mode, L, n1, ρ1, n2, ρ2)
         total_time += time
         println(file, "$L\t$time\t$total_time")  # Write time and total_time in two separate columns
     end
