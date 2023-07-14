6c65e34dbd0c090e8d21a0c4a5ac6bae76438d81
Ali Bagci <abagci.phys@gmail.com>
2023-07-14 12:08:43 +0300

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -26,9 +26,9 @@ function HyperRaFT(HyperD::HyperRaFD, L::Int, n1::arb, ρ1::arb, n2::arb, ρ2::a
         for s in 0 : L
             for s1 in 0 : s
                 time = @elapsed HyperD.HyperD(HyperD.mode, s1, n1, ρ1, n2, ρ2)
-                println(filet, "$s\t$time\t$total_time")
                 total_time += time
             end
+            println(filet, "$s\t$time\t$total_time")
             val = HyperD.HyperD(HyperD.mode, s, n1, ρ1, n2, ρ2)
             println(filev, "$s\t$val")
         end
